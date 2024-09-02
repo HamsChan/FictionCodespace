335df53f97b18b04e9c048416ad59f18ceaa4ac1
FictionFriend <78795430+YHamsChan@users.noreply.github.com>
2024-09-02 09:09:10 -0400

    Update StarTechLite.lua

--- StarTechLite.lua
+++ StarTechLite.lua
@@ -25,7 +25,11 @@ function Introduction()
 	if Choice == 1 then
 		Print_DeviceInfo()
 	else
-		AskingCommandAgain_Wrong()function AskingCommandAgain_Wrong()
+		AskingCommandAgain_Wrong()
+		
+	end
+end
+function AskingCommandAgain_Wrong()
 	print("╔═══[!!!!]═══════════════════════════════════╗")
 	print("  Error: Command Not Exist!")
 	print("  Date:"..os.date("%Y-%m-%d").." : ".."【"..os.date("%H:%M:%S").."】")
@@ -45,10 +49,6 @@ function Introduction()
 	else
 		AskingCommandAgain_Wrong()
 	end
-	--##
-end
-	end
-	--##
 end
 --lua StarTechLite.lua
 
@@ -78,6 +78,7 @@ function Print_DeviceInfo()
 	print("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n")
 	print("  Manufacturer: "..Manufacturer.."\n  Model: "..Model)
 	print("\n┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
+	os.execute("sleep 0.5")
 	AskingCommandAgain_Fine()
 end
 
