4246ff9a1dbfda08c5bfe45b9d0483bb3fe1d44e
FictionFriend <78795430+YHamsChan@users.noreply.github.com>
2024-09-03 17:14:59 +0700

    Update StarTechLite.lua

--- StarTechLite.lua
+++ StarTechLite.lua
@@ -1,86 +1,89 @@
-local Handle = io.popen("sudo dmidecode -s system-manufacturer")
-local Manufacturer = Handle:read("*a"):gsub("%s+$","") --gsub to remove gap of return/break or enter
-Handle:close()
-local Handle2 = io.popen("sudo dmidecode -s system-product-name")
-local Model = Handle2:read("*a"):gsub("%s+$","")
-Handle2:close()
-os.execute("clear")
+function MechanicReset()
+    RawTable = {
+        Type = io.popen("sudo dmidecode -s system-manufacturer"),
+        --gsub to remove gap of return/break or enter
+        Type2 = io.popen("sudo dmidecode -s system-product-name"),
+        Ip_Host = io.popen("ifconfig wlan0 | grep 'inet ' | awk '{print $2}'")
+    }
+    TypeDevice_Table = {
+        Type = RawTable.Type:read("*a"):gsub("%s+$",""),
+        Type2 = RawTable.Type2:read("*a"):gsub("%s+$","")
+    }
+    Ip_Table = {
+        Ip_Host = RawTable.Ip_Host:read("*a"):gsub("%s+$", "")
+    }
+    for i,v in pairs(RawTable) do
+        v:close() --Close the resource, important.
+    end
+    os.execute("clear")
+    Introduction()
+end
 
-function Introduction()
+function CommandListOutput()
+    --down is for command list
+    print("【1】Read System Of Yours. \n【2】None \n【3】None \n【4】None \n【5】None \n【6】None")
+    --##
+    os.execute("sleep 0.5")
+    print("\n")
+    print("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓")
+    --Reading down there for command
+    io.write("\nEnter Code: ")
+    local Choice = io.read("*n")
+    print("\n┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
+    if Choice == 1 then
+        Print_DeviceInfo()
+    else
+        AskingCommand(false)
+    end
+end
+
+function Introduction()--Appear when program work
+    os.execute("clear")
 	print("╔═══[✦✦✦✦]═══════════════════════════════════╗")
 	print("Project StarTech: Lite")
 	print("【v.0.1 | 03 Septempber 2024】")
 	print("Created By: 【✿FictionCyber✿】")
 	print("Powered with: Lua")
 	print("╚════════════════════════════════════════════╝")
-	--down is for command list
-	print("【1】Read System Of Yours. \n【2】None \n【3】None \n【4】None \n【5】None \n【6】None")
-	--##
-	os.execute("sleep 0.5")
-	print("\n")
-	print("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓")
-	--Reading down there for command
-	io.write("\nEnter Code: ")
-	local Choice = io.read("*n")
-	print("\n┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
-	if Choice == 1 then
-		Print_DeviceInfo()
-	else
-		AskingCommandAgain_Wrong()
-		
-	end
+	CommandListOutput()
 end
-function AskingCommandAgain_Wrong()
-	print("╔═══[!!!!]═══════════════════════════════════╗")
-	print("  Error: Command Not Exist!")
-	print("  Date:"..os.date("%Y-%m-%d").." : ".."【"..os.date("%H:%M:%S").."】")
-	print("╚════════════════════════════════════════════╝")
-	--down is for command list
-	print("【1】Read System Of Yours. \n【2】None \n【3】None \n【4】None \n【5】None \n【6】None")
-	print("[Press: CTRL+C To Quit.]")
-	--##
-	print("\n")
-	print("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓")
-	--Reading down there for command
-	io.write("\nEnter Code: ")
-	local Choice = io.read("*n")
-	print("\n┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
-	if Choice == 1 then
-		Print_DeviceInfo()
-	else
-		AskingCommandAgain_Wrong()
-	end
-end
+
+---------------------------------------------------
 --lua StarTechLite.lua
 
-function AskingCommandAgain_Fine()
-	print("╔═══[****]═══════════════════════════════════╗")
-	print("  Enter your command!")
-	print("╚════════════════════════════════════════════╝")
-	--down is for command list
-	print("【1】Read System Of Yours. \n【2】None \n【3】None \n【4】None \n【5】None \n【6】None")
-	print("[Press: CTRL+C To Quit.]")
-	--##
-	print("\n")
-	print("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓")
-	--Reading down there for command
-	io.write("\nEnter Code: ")
-	local Choice = io.read("*n")
-	print("\n┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
-	if Choice == 1 then
-		Print_DeviceInfo()
-	else
-		AskingCommandAgain_Wrong()
-	end
-	--##
+function AskingCommand(Condition)
+    os.execute("clear")
+    if Condition == true then
+        print("╔═══[****]═══════════════════════════════════╗")
+        print("  Enter your command!")
+        print("╚════════════════════════════════════════════╝")
+    else--Tells that condition of the last command was error.
+        print("╔═══[!!!!]═══════════════════════════════════╗")
+        print("  Error: Command Not Exist!")
+        print("  Date:"..os.date("%Y-%m-%d").." : ".."【"..os.date("%H:%M:%S").."】")
+        print("╚════════════════════════════════════════════╝")
+    end
+	CommandListOutput()
 end
 
+------------COMMAND LIST THAT ARE ACTIVE
 function Print_DeviceInfo()
+    os.execute("clear")
 	print("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n")
-	print("  Manufacturer: "..Manufacturer.."\n  Model: "..Model)
+	print("  Manufacturer: "..TypeDevice_Table.Type.."\n  Model: "..TypeDevice_Table.Type2)
 	print("\n┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
 	os.execute("sleep 0.5")
-	AskingCommandAgain_Fine()
+	AskingCommand(true)
 end
 
-Introduction()
+function Print_IP_Host()
+    os.execute("clear")
+	print("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n")
+    print(" Connected Wi-Fi IP Address: " .. Ip_Table.Ip_Host)
+	print(" ")
+	print("\n┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
+	os.execute("sleep 0.5")
+	AskingCommand(true)
+end
+
+MechanicReset()
